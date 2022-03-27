8fc400de5daa022416ad066a56057b53b562c1fb
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-27 22:33:12 +0900

    pages sections and animation

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const PageTrans = () => {
             e.target.classList.add('active');
         });
     });
+    //active sections
+    allSect.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from other sections
+            sectBtns.forEach(sec => sec.classList.remove('selected'));
+            e.target.classList.add('selected');
+        // hide other sections
+            section.forEach(sec => { sec.classList.remove('selected')});
+        
+        const element = document.getElementById(id);
+            element.classList.add('selected');
+        }
+    });
 };
 PageTrans();
 
